b41c2510fc96fedcce5f28bea1e8c516dd8b2d75
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-17 23:08:36 +0530

    Add files via upload

---
+++ codechef2.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef2        //problem Easy Pronunciation
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner sc = new Scanner(System.in);
+         int T=sc.nextInt();
+         while(T-->0) 
+         {
+            int N=sc.nextInt();
+            String S=sc.next();
+            int cnt=0;
+            boolean hard=false;
+            for(int i=0;i<N;i++) 
+            {
+                char ch=S.charAt(i);
+                if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') 
+                {
+                    cnt=0;
+                } 
+                else 
+                {
+                    cnt++;
+                    if(cnt>=4) 
+                    {
+                        hard=true;
+                        break;
+                    }
+                }
+            }
+            System.out.println(hard ? "NO" : "YES");
+        }
+	}
+}
