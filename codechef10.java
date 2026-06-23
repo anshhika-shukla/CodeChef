38f3ea92ec574902e60054f800d125183c5b7a00
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-23 22:36:47 +0530

    Add files via upload

---
+++ codechef10.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Main      //problem - Water Filling
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();    
+        while(t-->0) 
+        {
+            int b1 = sc.nextInt();
+            int b2 = sc.nextInt();
+            int b3 = sc.nextInt();    
+            int emptyCount = 0;
+            if (b1 == 0) emptyCount++;
+            if (b2 == 0) emptyCount++;
+            if (b3 == 0) emptyCount++;
+            if (emptyCount >= 2) 
+            {
+                System.out.println("Water filling time");
+            }
+            else 
+            {
+                System.out.println("Not now");
+            }
+        }
+    }
+}
