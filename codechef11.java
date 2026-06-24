70e1cecd7116398e4cbe2a466b990a00ab692916
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-24 22:47:05 +0530

    Add files via upload

---
+++ codechef11.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class codechef11    //problem-Carrot Collection
+{
+    public static void main(String anshika[]) 
+    {
+        Scanner s=new Scanner(System.in);    
+        int t=s.nextInt();
+        while(t-->0) 
+        {
+            int n=s.nextInt();
+            int l=s.nextInt();
+            int r=s.nextInt();
+            int arr[]=new int[(n+1)]; 
+            for(int i=1;i<=n;i++) 
+            {
+                arr[i]=s.nextInt();
+            }
+            int x=0;
+            for(int i=1;i<l;i++) 
+            {
+                x=x+arr[i];
+            }
+            int y=0;
+            for(int i=r+1;i<=n;i++) 
+            {
+                y=y+arr[i];
+            }
+            System.out.println(Math.max(x,y));
+        }
+    }
+}
