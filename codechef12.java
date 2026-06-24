946aafe6f55ba51b0ef29dfcbccb8be1c4ec9216
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-24 22:47:24 +0530

    Add files via upload

---
+++ codechef12.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class codechef12    //problem-Passing Chain
+{
+    public static void main(String anshika[]) 
+    {
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        while(t-->0) 
+        {
+            int n=s.nextInt();
+            int k=s.nextInt();
+            int ans=1;
+            while((ans+k)<=n) 
+            {
+                ans=ans+k;
+            }
+            System.out.println(ans);
+        }
+    }
+}
