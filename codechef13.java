b3902ab6681ee4e55ca4cd928674aef63e314786
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-24 22:48:00 +0530

    Add files via upload

---
+++ codechef13.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class codechef13     //problem-Elections in Chefland
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int Xa=sc.nextInt();
+            int Xb=sc.nextInt();
+            int Xc=sc.nextInt();
+            if(Xa>50)
+            {
+                System.out.println("A");
+            }
+            else if(Xb>50)
+            {
+                System.out.println("B");
+            }
+            else if(Xc>50)
+            {
+                System.out.println("C");
+            }
+            else
+            {
+                System.out.println("NOTA");
+            }
+        }
+    }
+}
