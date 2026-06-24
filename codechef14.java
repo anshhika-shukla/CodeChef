6156292751c4e942c97f0fa694ac028c30996872
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-24 22:48:16 +0530

    Add files via upload

---
+++ codechef14.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class codechef14    //problem-Sale Season
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        while(T-->0)
+        {
+            int X=sc.nextInt();
+            int cost=0;
+            if(X<=100)
+            {
+                cost=X;
+            }
+            else if(X>100 && X<=1000)
+            {
+                cost=X-25;
+            }
+            else if(X>1000 && X<=5000)
+            {
+                cost=X-100;
+            }
+            else 
+            {
+                cost=X-500;
+            }
+            System.out.println(cost);
+        }
+    }
+}
