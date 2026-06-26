0f313ebabce4ce4e1ff83f124efb0d446f25c814
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-26 23:52:23 +0530

    codechef16.java

--- codechef16.java
+++ codechef16.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class codechef16
 {
-    public static void main(String args[])                         // Problem Minimum Pizzas
+    public static void main(String args[])                         //Problem Minimum Pizzas
     {
         Scanner sc=new Scanner(System.in);    
         int t=sc.nextInt();      
