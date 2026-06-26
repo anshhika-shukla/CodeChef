ea20186314cff6923a783274e9c47841d4b34362
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-26 23:51:25 +0530

    Add files via upload

---
+++ codechef17.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class codechef17   //problem-Chefland Games
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0) 
+        {
+            int r1 = sc.nextInt();
+            int r2 = sc.nextInt();
+            int r3 = sc.nextInt();
+            int r4 = sc.nextInt();
+            if(r1==0 && r2==0 && r3==0 && r4==0) 
+            {
+                System.out.println("IN");
+            } 
+            else 
+            {
+                System.out.println("OUT");
+            }
+        }
+    }
+}
