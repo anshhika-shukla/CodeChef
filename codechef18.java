a775e35a1b1f60e1a7077f12393fbcaa1a8fa4c6
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-27 23:23:05 +0530

    Add files via upload

---
+++ codechef18.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class codechef18    //problem Expert Setter
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0) 
+        {
+            int x=sc.nextInt(); 
+            int y=sc.nextInt(); 
+            if(2*y >= x) 
+            {
+                System.out.println("YES");
+            } 
+            else 
+            {
+                System.out.println("NO");
+            }
+        }
+    }
+}
