275f6035acfdf280de68d45bc94961953816f82c
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-29 22:52:27 +0530

    Add files via upload

---
+++ codechef23.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class codechef23  //problem CodeChef XP
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int P = sc.nextInt();
+        int E = sc.nextInt();
+        System.out.println((A * P) + (B * E));
+    }
+}
