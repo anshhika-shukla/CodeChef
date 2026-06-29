646d623578bb987ce1cfb355c82ff7aac3e46d93
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-29 22:52:08 +0530

    Add files via upload

---
+++ codechef22.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class codechef22  //problem Leader of an Array
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        long a[]=new long[n];
+        for(int i=0;i<n;i++) 
+        {
+            a[i]=sc.nextLong();
+        }
+        long c[]=new long[n];
+        int count=0;
+        long max=Long.MIN_VALUE;
+        for(int i=n-1;i>=0;i--) 
+        {
+            if(a[i]>max) 
+            {
+                c[count++]=a[i];
+                max=a[i];
+            }
+        }
+        for(int i=count-1;i>=0;i--) 
+        {
+            System.out.print(c[i]+" ");
+        }
+    }
+}
