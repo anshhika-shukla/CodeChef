1c02be1ce92fbf48a1c2c9257d5144ac47f04dd8
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-30 23:23:34 +0530

    Add files via upload

---
+++ codechef24.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class codechef24   //problem Chef and Candies
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            int x = scanner.nextInt();
+            
+            
+            if (x >= n) {
+                System.out.println(0);
+            } else {
+                int needed = n - x;
+               
+                int packets = (needed + 3) / 4;
+                System.out.println(packets);
+            }
+        }
+    }
+}
