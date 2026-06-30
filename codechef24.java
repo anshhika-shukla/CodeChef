bf50639aefc7380a9e78ceca08121c171a88a4f5
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-30 23:24:09 +0530

    codechef24.java

--- codechef24.java
+++ codechef24.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class codechef24   //problem Chef and Candies
+public class codechef24   //Problem Chef and Candies
 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
