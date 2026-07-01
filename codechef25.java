8b71054965638545fd910129a110fe67ea60a5f9
Anshika Shukla <shuklaanshika145@gmail.com>
2026-07-01 22:40:59 +0530

    Add files via upload

---
+++ codechef25.java
@@ -0,0 +1,22 @@
+
+
+import java.util.*;
+class codechef25   //problem The Three Topics
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int x=sc.nextInt();    
+        if(x==a || x==b || x==c) 
+        {
+            System.out.println("Yes");
+        } 
+        else 
+        {
+            System.out.println("No");
+        }
+    }
+}
